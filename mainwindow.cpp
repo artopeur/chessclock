baf7b071bbe81fce9abc01a12b6ffdebc9c51c56
artopeur <arto.peurasaari@gmail.com>
2024-12-22 22:18:18 +0200

    Chessclock with QT

---
+++ mainwindow.cpp
@@ -0,0 +1,213 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->progressBar->setMaximum(100);
+    ui->progressBar_2->setMaximum(100);
+    ui->progressBar->setValue(100);
+    ui->progressBar_2->setValue(100);
+    updateProgressBar();
+    setPlayerInfoText("", 1);
+    setGameInfoText("Choose time",2);
+    connect(ui->startGameButton, SIGNAL(clicked(bool)), this, SLOT(startGame()));
+    connect(ui->gameMode1,SIGNAL(clicked(bool)),this,SLOT(playValue()));
+    connect(ui->gameMode2,SIGNAL(clicked(bool)),this,SLOT(playValue()));
+    connect(ui->switchPlayerButton, SIGNAL(clicked(bool)),this,SLOT(switchPlayer()));
+    connect(ui->switchPlayerButton2, SIGNAL(clicked(bool)), this, SLOT(switchPlayer()));
+    connect(ui->stopGameButton, SIGNAL(clicked(bool)), this, SLOT(stopGame()));
+
+    pQTimer = new QTimer();
+    //connect(pQTimer, SIGNAL(destroyed(QObject*)), this, SLOT(updateProgressBar()));
+    connect(pQTimer, SIGNAL(timeout()),this,SLOT(timerRun()));
+
+}
+
+MainWindow::~MainWindow()
+{
+
+    delete ui;
+    qDebug()<<"ui deleted";
+    delete pQTimer;
+    qDebug() << "timer deleted";
+}
+
+void MainWindow::timeout()
+{
+    if(currentPlayer == 1) {
+        setGameInfoText("Player 2 won, timeout.",2);
+        pQTimer->stop();
+        currentPlayer = 0;
+    }
+    else if(currentPlayer == 2){
+        setGameInfoText("Player 1 won, timout.",2);
+        pQTimer->stop();
+        currentPlayer = 0;
+    }
+    else {
+        setGameInfoText("Game has stopped",4);
+    }
+    //stopGame();
+}
+
+void MainWindow::updateProgressBar()
+{
+    ui->progressBar->setValue(player1Time);
+    ui->progressBar_2->setValue(player2Time);
+}
+void MainWindow::setGameInfoText(QString text, short size)
+{
+    QFont font;
+    ui->label->font();
+
+    switch(size) {
+    case 1:
+        font.setPointSize(10);
+        break;
+    case 2:
+        font.setPointSize(20);
+        break;
+    case 3:
+        font.setPointSize(30);
+        break;
+    case 4:
+        font.setPointSize(40);
+        break;
+    default:
+        font.setPointSize(10);
+        break;
+    }
+
+
+    ui->label->setFont(font);
+    ui->label->setText(text);
+}
+
+void MainWindow::setPlayerInfoText(QString text, short size)
+{
+    QFont font;
+    ui->label_2->font();
+
+    switch(size) {
+    case 1:
+        font.setPointSize(10);
+        break;
+    case 2:
+        font.setPointSize(20);
+        break;
+    case 3:
+        font.setPointSize(30);
+        break;
+    case 4:
+        font.setPointSize(40);
+        break;
+    default:
+        font.setPointSize(10);
+        break;
+    }
+
+
+    ui->label_2->setFont(font);
+    ui->label_2->setText(text);
+}
+
+void MainWindow::startGame() {
+    setGameInfoText("Game ongoing",4);
+    setPlayerInfoText("choose who starts",2);
+    //pQTimer->startTimer(1000);
+    pQTimer->start(1000);
+
+}
+void MainWindow::stopGame() {
+    pQTimer->stop();
+    currentPlayer=0;
+    //player1Time = 0;
+    //player2Time = 0;
+    //gameTime = 0;
+    setGameInfoText("Gave Over. Select time, press start.",2);
+}
+void MainWindow::playValue()
+{
+    QPushButton * button = qobject_cast<QPushButton*>(sender());
+    QString name= button->objectName();
+    qDebug() << "Button name: " << name;
+
+    if(name == "gameMode1" && currentPlayer == 0) {
+        setGameInfoText("Button clicked : 120 seconds",2);
+        //test time faster
+        //gameTime=6;
+        gameTime=2*60;
+        player1Time=gameTime;
+        player2Time=gameTime;
+        ui->progressBar->setMaximum(gameTime);
+        ui->progressBar_2->setMaximum(gameTime);
+    }
+    else if(name == "gameMode2" && currentPlayer == 0){
+        setGameInfoText("Button clicked : 5 minutes", 2);
+        gameTime=5*60;
+        player1Time=gameTime;
+        player2Time=gameTime;
+        ui->progressBar->setMaximum(gameTime);
+        ui->progressBar_2->setMaximum(gameTime);
+    }
+
+}
+
+void MainWindow::timerRun()
+{
+    qDebug() << "timeout";
+    qDebug() << "Player 1: " << player1Time;
+    qDebug() << "Player 2: " << player2Time;
+
+    if(player2Time>0 && currentPlayer==2) {
+        setGameInfoText("Game ongoing",4);
+        player2Time--;
+    }
+    else if(player1Time>0 && currentPlayer==1) {
+        setGameInfoText("Game ongoing",4);
+        player1Time--;
+    }
+    else {
+        if(currentPlayer == 2) {
+            // player 1 won
+            timeout();
+        }
+        else if(currentPlayer == 1) {
+            // player 2 won
+            timeout();
+        }
+        else {
+            //not started yet
+            //setGameInfoText("Start Game by choosing player",1);
+        }
+        //stopGame();
+    }
+
+
+    updateProgressBar();
+
+}
+
+void MainWindow::switchPlayer()
+{
+
+    QPushButton * button = qobject_cast<QPushButton*>(sender());
+    QString name= button->objectName();
+    qDebug() << "Button name: " << name;
+
+    if(name == "switchPlayerButton") {
+        qDebug()<<"player 1";
+        setPlayerInfoText("Player 2 turn", 2);
+        currentPlayer = 2;
+    }
+    else if(name == "switchPlayerButton2") {
+        qDebug()<<"player 2";
+        setPlayerInfoText("Player 1 turn", 2);
+        currentPlayer = 1;
+    }
+
+    qDebug()<<"switch: " + name;
+}
---
+++ mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void timeout();
+private:
+    Ui::MainWindow *ui;
+    short player1Time = 0;
+    short player2Time = 0;
+    short currentPlayer = 0;
+    short gameTime = 0;
+
+    //QObject *pQTimer;
+    QTimer * pQTimer;
+
+    void updateProgressBar();
+    void setGameInfoText(QString, short);
+    void setPlayerInfoText(QString, short);
+
+private slots:
+    void startGame();
+    void stopGame();
+    void playValue();
+    void timerRun();
+    void switchPlayer();
+};
+#endif // MAINWINDOW_H
